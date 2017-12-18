974ce834283014083f92d40ec3565d07f28682ca
fabiomor <fabiomor@gmail.com>
2017-12-18 16:00:51 +0100

    filter

--- src/weatherstations/station-gallery.controller.js
+++ src/weatherstations/station-gallery.controller.js
@@ -4,12 +4,36 @@
 angular.module('WeatherStations')
 .controller('StationGalleryController', StationGalleryController);
 
+StationGalleryController.$inject = ['$scope','images','dateRangeFilter', 'hourFilter'];
+function StationGalleryController($scope, images, dateFilter, hourFilter) {
+  let sg = this;
+  $scope.date = {};
+  $scope.date.start = new Date("2017-12-1")
+  $scope.date.end = new Date("2017-12-2")
+  sg.hourSelection = [
+    {id: 0, value:'All'},
+    {id: 1, value:'10'},
+    {id: 2, value:'13'},
+    {id: 3, value:'15'}
+  ];
+  $scope.date.hour = sg.hourSelection[0];
 
-StationGalleryController.$inject = ['images'];
-function StationGalleryController(images) {
-  var sg = this;
-  sg.images = images.data;
-  console.log(images);
+  if($scope.date.start) {
+    sg.images = dateFilter(images.data, $scope.date.start, $scope.date.end);
+  }
+  else {
+    sg.images = images.data;
+  }
+
+  sg.dateChanged = function () {
+    sg.images = dateFilter(images.data, $scope.date.start, $scope.date.end);
+    sg.images = hourFilter(sg.images, $scope.date.hour.value);
+  }
+
+  sg.hourChanged = function() {
+    sg.images = dateFilter(images.data, $scope.date.start, $scope.date.end);
+    sg.images = hourFilter(sg.images, $scope.date.hour.value);
+  }
 }
 
 })();
